5ffb0001219c95eed5834c5be04a0b26e44b1705
denetti <denetti24@gmail.com>
2021-03-30 23:18:20 +0300

    3/30/21 hw

---
+++ W4/4-7 sumnotsum.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+
+def sum(a, b):
+    if b == 0:
+        return a
+    return sum(a, b - 1) + 1
+
+print(sum(a, b))
---
+++ W4/4-8 fastrowN.py
@@ -0,0 +1,13 @@
+a = float(input())
+n = int(input())
+
+
+def power(a, n):
+    if n == 0:
+        return 1
+    if n % 2 != 0:
+        return a * power(a, n - 1)
+    else:
+        return power(a ** 2, n / 2)
+
+print(power(a, n))
